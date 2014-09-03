2ba45d9d35b0f2151d4b23787b5b9497ab97698b
Frederick C. Lee <frederick.c.lee@gmail.com>
2014-09-02 18:41:17 -0700

    Swift Update
    Enhanced Palindrome with a continuous loop till a nil entry.

--- main.swift
+++ main.swift
@@ -7,28 +7,41 @@
 
 import Foundation
 
-println("Palindrome: A word that is same either backwards or forwards spelling; e.g., 'Bob'")
-println("Enter a word that you think is a Palidrome: ")
-
-let myData:NSData = NSFileHandle.fileHandleWithStandardInput().availableData
-let str:NSString? = NSString(data: myData, encoding: NSUTF8StringEncoding)
-
-let middleIndex = str!.length/2
-var x:Int = 0
-var y:Int = str!.length - 2  // ...ignore carriage return character and set for zero (0) based index.
-
-let substr = str!.substringWithRange(NSRange(location: 1,length: 1))
-var matched:Bool = false
+var str:NSString?
+var matched:Bool
 
 do {
-    let leftChar = str!.substringWithRange(NSRange(location: x++,length: 1))
-    let rightChar = str!.substringWithRange(NSRange(location: y--,length: 1))
-    matched = (leftChar == rightChar)
-} while x == middleIndex
+    println("Palindrome: A word that is same either backwards or forwards spelling; e.g., 'Bob'")
+    println("Enter a word that you think is a Palidrome (enter nothing to exit): ")
+    
+    let myData:NSData = NSFileHandle.fileHandleWithStandardInput().availableData
+    str = NSString(data: myData, encoding: NSUTF8StringEncoding)
+    
+    matched = false
+    
+    if str!.length > 1 {
+        let middleIndex = str!.length/2
+        var x:Int = 0
+        var y:Int = str!.length - 2  // ...ignore carriage return character and set for zero (0) based index.
+        
+        let substr = str!.substringWithRange(NSRange(location: 1,length: 1))
+        
+        do {
+            let leftChar = str!.substringWithRange(NSRange(location: x++,length: 1))
+            let rightChar = str!.substringWithRange(NSRange(location: y--,length: 1))
+            matched = (leftChar == rightChar)
+            if !matched {
+                break
+            }
+        } while x != middleIndex
+        
+        if matched {
+            println("...is a palindrome.\n\n")
+        } else {
+            println("...is *NOT* a palindrome.\n\n")
+        }
+    }
+} while (str!.length > 1)
 
-if matched {
-    print("\n\(str!) is a Palidrome!\n\n")
-} else {
-    print("\n\(str!) is *NOT* a Paladrome.\n\n")
-}
+println("La Fin.")
 
